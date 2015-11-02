4974c72eb93b5448d2bd3278dea7d99edf7a6220
nanyufei <283628073@qq.com>
2015-11-02 20:47:16 +0800

    me PickerView

---
+++ pickView/pickView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  pickView
+//
+//  Created by qingyun on 15/11/2.
+//  Copyright (c) 2015年 qingyun.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIPickerView
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
