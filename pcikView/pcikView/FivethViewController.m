c1d8f72a47e5e5001996657178c1adfdd06d7251
nanyufei <283628073@qq.com>
2015-11-03 17:09:45 +0800

    自制微信

---
+++ pcikView/pcikView/FivethViewController.m
@@ -0,0 +1,82 @@
+//
+//  FivethViewController.m
+//  pcikView
+//
+//  Created by qingyun on 15/11/2.
+//  Copyright (c) 2015年 qingyun.com. All rights reserved.
+//
+
+#import "FivethViewController.h"
+#define screenW [UIScreen mainScreen].bounds.size.width
+#define screenH [UIScreen mainScreen].bounds.size.height
+
+@interface FivethViewController ()<UIPickerViewDataSource,UIPickerViewDelegate,UIPickerViewAccessibilityDelegate>
+@property (nonatomic,strong) NSArray *datas;
+@property (nonatomic,strong) UIPickerView *pickView;
+@end
+
+@implementation FivethViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setPickerView];
+    
+}
+-(void)setPickerView
+{
+    UIPickerView *pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 50,screenW , 150)];
+    [self.view addSubview:pickerView];
+    pickerView.delegate = self;
+    pickerView.dataSource = self;
+    _pickView = pickerView;
+    
+    _datas = @[@"apple",@"bar",@"cherry",@"crown",@"lemon",@"seven"];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(150, 400, 75, 40);
+    [self.view addSubview:btn];
+    [btn setTitle:@"摇一摇" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+}
+-(void)btnClick:(UIButton *)btn
+{
+    
+    for (int i = 0; i < 5; i ++) {
+        NSInteger selectedRow = round(random()%_datas.count);
+        [_pickView selectRow:selectedRow inComponent:i animated:YES];
+    }
+    
+
+}
+
+
+#pragma mark - UIPickerViewDataSource
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 5;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 6;
+}
+#pragma mark - UIPickerViewDelegate
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
+{
+    UIImageView *imageView = [[UIImageView alloc] init];
+    NSString *imageName = [NSString stringWithString:_datas[row]];
+    imageView.image = [UIImage imageNamed:imageName];
+    return imageView;
+    
+}
+-(CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component
+{
+    return 70;
+}
+-(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+    return 60;
+}
+
+
+@end
